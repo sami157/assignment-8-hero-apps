968124aa2aa8351a45a6de207c20f8d95a03898d
Tazkia Malik <99531375+sami157@users.noreply.github.com>
2025-10-10 16:35:13 +0600

    fix

--- src/components/InstalledAppCard.jsx
+++ src/components/InstalledAppCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const InstalledAppCard = () => {
+const InstalledAppCard = ({ image, title, downloads, rating }) => {
   return (
     <div className='w-full p-[16px] bg-white rounded-[8px] flex items-center justify-between'>
       <div className='flex gap-[16px] items-center'>
-        <img className='w-[80px] rounded-[4px]' src="src\assets\error-404.png" alt="" />
+        <img className='w-[80px] rounded-[4px]' src={image} alt="" />
         <div className='flex flex-col gap-[16px]'>
-            <p className='text-[20px] font-medium'>App Name</p>
+            <p className='text-[20px] font-medium'>{title}</p>
             <div className='flex gap-[16px]'>
                 <div className='flex gap-[4px] items-center'>
                     <img className='w-[12px]' src="src\assets\download.png" alt="" />
@@ -14,9 +14,9 @@ const InstalledAppCard = () => {
                 </div>
                 <div className='flex gap-[4px] items-center'>
                     <img className='w-[12px]' src="src\assets\star.png" alt="" />
-                    <p className='text-[#FF8811]'>5</p>
+                    <p className='text-[#FF8811]'>{downloads}</p>
                 </div>
-                <p className='text-[#627382]'>258 MB</p>
+                <p className='text-[#627382]'>{rating}</p>
             </div>
         </div>
       </div>
